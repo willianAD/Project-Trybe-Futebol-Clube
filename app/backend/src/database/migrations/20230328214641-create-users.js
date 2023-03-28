09cb15db090d69d325279379cd0d51c2bf5b8809
willianAD <willian.afonso013@gmail.com>
2023-03-28 18:58:48 -0300

    Requisito 6 - create table users

---
+++ app/backend/src/database/migrations/20230328214641-create-users.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    return queryInterface.dropTable('users');
+  },
+};
---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,37 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  underscored: true,
+});
+
+export default User;
