7ccaa38a2e9a7643774791bbb3b07e405ac60ba5
willianAD <willian.afonso013@gmail.com>
2023-03-29 23:43:45 -0300

    Iniciando implementacao testes de Teams

---
+++ app/backend/src/tests/interfaces/IErrorMsg.ts
@@ -0,0 +1,3 @@
+export default interface IErrorMsg { 
+  message: string,
+}
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,100 +1,45 @@
-// import * as chai from 'chai';
-// import * as sinon from 'sinon';
-// // @ts-ignore
-// import chaiHttp = require('chai-http');
-// const { expect } = chai;
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+const { expect } = chai;
 
-// import { app } from '../app';
+import { app } from '../app';
 
-// import { Response } from 'superagent';
-// import Team from '../database/models/Team';
+import { Response } from 'superagent';
+import Team from '../database/models/Team';
+import { mockTeam, bestTeam, errorMessage } from './mocks/teamsMock';
+// import IErrorMsg from './interfaces/IErrorMsg';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const mockTeam = [
-//   {
-//     "id": 1,
-//     "teamName": "Avaí/Kindermann"
-//   },
-//   {
-//     "id": 2,
-//     "teamName": "Bahia"
-//   },
-//   {
-//     "id": 3,
-//     "teamName": "Botafogo"
-//   },
-//   {
-//     "id": 4,
-//     "teamName": "Corinthians"
-//   },
-//   {
-//     "id": 5,
-//     "teamName": "Cruzeiro"
-//   },
-//   {
-//     "id": 6,
-//     "teamName": "Ferroviária"
-//   },
-//   {
-//     "id": 7,
-//     "teamName": "Flamengo"
-//   },
-//   {
-//     "id": 8,
-//     "teamName": "Grêmio"
-//   },
-//   {
-//     "id": 9,
-//     "teamName": "Internacional"
-//   },
-//   {
-//     "id": 10,
-//     "teamName": "Minas Brasília"
-//   },
-//   {
-//     "id": 11,
-//     "teamName": "Napoli-SC"
-//   },
-//   {
-//     "id": 12,
-//     "teamName": "Palmeiras"
-//   },
-//   {
-//     "id": 13,
-//     "teamName": "Real Brasília"
-//   },
-//   {
-//     "id": 14,
-//     "teamName": "Santos"
-//   },
-//   {
-//     "id": 15,
-//     "teamName": "São José-SP"
-//   },
-//   {
-//     "id": 16,
-//     "teamName": "São Paulo"
-//   }
-// ]
-
-
-// describe('Testando Teams', function () {
-//   afterEach(()=>{ sinon.restore(); });
-
-//   it('...', async function () {
-//     const res = {};
-//     const req = {};
-
-//     sinon.stub(Team, 'findAll').resolves(mockTeam);
-
-//     await chai.request(app).get('/teams');
+describe('Testando  a rota /teams', function () {
+  it('Verifica se retorna um status 200 e todos os times', async () => {
+    sinon.stub(Team, 'findAll').resolves(mockTeam as Team[]);
     
-//     expect(res.status).to.be.equal(200);
-//     expect(res.json).to.be.deep.equal(mockTeam);
-//   });
+    const res: Response = await chai.request(app).get('/teams');
+    
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.deep.equal(mockTeam);
+  });
 
-//   it('Seu sub-teste', () => {
-//     expect(false).to.be.eq(true);
-//   });
-// });
+  it('Verifica se retorna um status 200 e busca por um id', async () => {
+    sinon.stub(Team, 'findByPk').resolves(bestTeam as Team);
+    
+    const res: Response = await chai.request(app).get('/teams/4');
+    
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.deep.equal(bestTeam);
+  });
+
+  it('Verifica se retorna um status 404 e uma menssagem de erro', async () => {
+    sinon.stub(Team, 'findByPk').resolves();
+    
+    const res: Response = await chai.request(app).get('/teams/500');
+    
+    expect(res.status).to.be.equal(404);
+    expect(res.body).to.be.deep.equal(errorMessage);
+  });
+
+  afterEach(()=>{ sinon.restore() });
+});
